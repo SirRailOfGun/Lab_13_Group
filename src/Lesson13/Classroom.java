c23e94a4b38bc94ca6f1aa33a3ccf1bd4766d9d4
rjsnyder2020 <94650497+rjsnyder2020@users.noreply.github.com>
2023-04-13 10:36:12 -0400

    Real files
    Saved and now they should work

--- src/Lesson13/Classroom.java
+++ src/Lesson13/Classroom.java
@@ -1,5 +1,29 @@
 package Lesson13;
 
-public class Classroom {
+public class Classroom extends Room{
+	public int numOfChairs;
+	
+	Classroom(int area) {
+		super(area);
+		
+	}
+	
+	Classroom(int area, int chairs){
+		super(area);
+		this.numOfChairs = chairs;
+	}
+	
+	public int getNumOfChairs() {return this.numOfChairs;}
+	public void setNumOfChairs(int numberChairs) {this.numOfChairs = numberChairs;}
+	
+	public int getCapacity() {
+		int capacity = this.numOfChairs;
+		return capacity;
+	}
+	
+	public String toString() {
+		return "Area in Square Feet: " + this.area + "\nCapacity: " + this.getCapacity()
+		+ "\nNumber of Chairs: " + this.numOfChairs;
+	}
 
 }
