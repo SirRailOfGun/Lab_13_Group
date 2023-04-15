08bdcb53633be7b8b192b9e31e74d131778f003f
rjsnyder2020 <94650497+rjsnyder2020@users.noreply.github.com>
2023-04-15 15:39:32 -0400

    Added javadoc

--- src/Lesson13/Classroom.java
+++ src/Lesson13/Classroom.java
@@ -1,26 +1,44 @@
 package Lesson13;
-
+/**
+ * Class extension from room that returns based on a classroom setting
+ * @author Robert
+ *
+ */
 public class Classroom extends Room{
 	public int numOfChairs;
-	
+	/**
+	 * Default constructor that uses the superclass area
+	 * @param area
+	 */
 	Classroom(int area) {
 		super(area);
 		
 	}
-	
+	/**
+	 * Constructor that takes numOfChairs and superclass area as parameters
+	 * @param area
+	 * @param chairs
+	 */
 	Classroom(int area, int chairs){
 		super(area);
 		this.numOfChairs = chairs;
 	}
-	
+	/**
+	 * Getter and setter for the number of chairs in the room
+	 * @return
+	 */
 	public int getNumOfChairs() {return this.numOfChairs;}
 	public void setNumOfChairs(int numberChairs) {this.numOfChairs = numberChairs;}
-	
+	/**
+	 * Gets the capacity based on the number of chairs
+	 */
 	public int getCapacity() {
 		int capacity = this.numOfChairs;
 		return capacity;
 	}
-	
+	/**
+	 * Returns a string using the area, capacity, and number of chairs
+	 */
 	public String toString() {
 		return "Area in Square Feet: " + this.area + "\nCapacity: " + this.getCapacity()
 		+ "\nNumber of Chairs: " + this.numOfChairs;
