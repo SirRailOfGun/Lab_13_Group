94db3a758006eb5416b0bb6a86b22c2a79456cbf
SirRailOfGun <Mike1011031@gmail.com>
2023-04-16 15:15:24 -0400

    add stringSet checkers

--- src/Lesson13/Control.java
+++ src/Lesson13/Control.java
@@ -1,9 +1,19 @@
 package Lesson13;
 
+import java.util.Scanner;
+
+import Lab_12.StringSet;
+
 public class Control {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		testRooms();
+		testStrings();
+	}
+	
+	static void testRooms()
+	{
 		Room room = new Room(0);
 		Classroom classroom = new Classroom(0);
 		Elevator elevator = new Elevator(0);
@@ -41,7 +51,22 @@ public class Control {
 		System.out.println(elevator.getCapacity());
 		System.out.println(elevator.getSquareFeet());
 		System.out.println(elevator.getFloor());
-		
 	}
 
+	static void testStrings()
+	{
+		StringSet myClass = new StringSet();
+		Scanner scan = new Scanner(System.in);
+		System.out.print("How many strings will you enter? ");
+		int numStr = scan.nextInt();
+		scan.nextLine();
+		for (int i = 0; i < numStr; i++)
+		{
+			myClass.add(scan.nextLine());
+		}
+		System.out.print("\n\n\nsize = ");
+		System.out.print(myClass.size());
+		System.out.print("\nnumber of characters = ");
+		System.out.print(myClass.numChars());
+	}
 }
